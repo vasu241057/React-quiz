2384278c4007a0f11fdea51200549395ed479070
Vasu Khandelwal <vasu241057@gmail.com>
2023-01-05 19:54:09 +0530

    Updates

---
+++ precache-manifest.48181d4a74dc67036a53b9cd3bedb0c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "264322804e7288d78311",
+    "url": "/React-quiz/static/css/main.b7bd2837.chunk.css"
+  },
+  {
+    "revision": "264322804e7288d78311",
+    "url": "/React-quiz/static/js/main.26432280.chunk.js"
+  },
+  {
+    "revision": "71da1cdb2829ee0c1df5",
+    "url": "/React-quiz/static/css/1.e50a3153.chunk.css"
+  },
+  {
+    "revision": "71da1cdb2829ee0c1df5",
+    "url": "/React-quiz/static/js/1.71da1cdb.chunk.js"
+  },
+  {
+    "revision": "17a8136be6fc1ba8e5eb",
+    "url": "/React-quiz/static/js/runtime~main.17a8136b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-quiz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10d815e516b5f4dd27b4ee7d5b5ac629",
+    "url": "/React-quiz/index.html"
+  }
+];
